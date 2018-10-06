c752dbeb0fb608aae842bc2a320a1502bbaf7694
Matheus Hernandes <midia.matheus@gmail.com>
2018-10-06 17:11:26 -0300

    es6-fy everything, also register hbs helpers globally

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-var path = require('path');
-var fs = require('fs');
-var handlebars = require('handlebars');
+const path = require('path')
+const fs = require('fs')
+const handlebars = require('handlebars')
 
-require('./helpers')(hexo);
+require('./helpers')(hexo)
 
-function handlebarsRenderer(data, locals) {
-  var template = handlebars.compile(data.text),
-    helperDir = path.join(hexo.theme_dir, 'helper');
+const handlebarsRenderer = (data, locals) => {
+  const template = handlebars.compile(data.text)
+  const helperDir = path.join(hexo.theme_dir, 'helper')
 
-  var partialDir = path.join(hexo.theme_dir, 'layout', 'partials');
-  var partials;
-  
+  const partialDir = path.join(hexo.theme_dir, 'layout', 'partials')
+  let partials
+
   try {
-    partials = fs.readdirSync(partialDir);
+    partials = fs.readdirSync(partialDir)
   } catch (e) {
     // if this fails, there just aren't any partials. No problem.
   }
 
   if (partials) {
     partials.forEach(function (fname) {
-      if (fname.split('.').slice(-1)[0] !== 'hbs') return;
-      handlebars.registerPartial(fname.split('.')[0], fs.readFileSync(path.join(partialDir, fname)).toString());
-    });
+      if (fname.split('.').slice(-1)[0] !== 'hbs') {
+        return
+      }
+
+      handlebars.registerPartial(fname.split('.')[0], fs.readFileSync(path.join(partialDir, fname)).toString())
+    })
   }
 
-  var helpers = {};
+  let helpers = {}
   try {
-    helpers = require(helperDir)(hexo);
-  } catch(e) {
-      // no additional helpers available. No problem.
+    helpers = require(helperDir)(hexo)
+  } catch (e) {
+    // no additional helpers available. No problem.
   }
 
-  return template(locals, {
-    helpers: helpers
-  });
+  Object.keys(helpers).forEach(k => handlebars.registerHelper(k, helpers[k]))
+
+  return template(locals)
 }
 
-['hbs', 'handlebars'].forEach(function(extension) {
-  hexo.extend.renderer.register(extension, 'html', handlebarsRenderer, true);
-});
+['hbs', 'handlebars'].forEach(ext => {
+  hexo.extend.renderer.register(ext, 'html', handlebarsRenderer, true)
+})
 
-exports.handlebars = handlebars;
+exports.handlebars = handlebars
